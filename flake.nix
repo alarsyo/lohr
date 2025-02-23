37f99e9c6e48bb819a5ca811f7b0287fcc4447d3
Antoine Martin <antoine@alarsyo.net>
2025-02-23 21:02:35 +0100

    flake: update all inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
